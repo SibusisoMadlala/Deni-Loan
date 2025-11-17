3ac8c9c9cb671dce36e96afad090aa4eea6591b7
SibusisoMadlala <87149996+SibusisoMadlala@users.noreply.github.com>
2025-11-17 09:24:03 +0200

    Update alt text for logo image in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export function Navbar() {
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="flex items-center space-x-2">
             <div className="w-16 h-16 rounded-lg flex items-center justify-center">
-              <img src="../assets/DeniLogo.png" alt="Heni" />
+              <img src="../assets/DeniLogo.png" alt="DeniLoans" />
             </div>
             
           </Link>
